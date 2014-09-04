06c89461ec12dd9c940ea6d1085c5483d3aa0250
Danilo Makoto Ikuta <danilo0195@hotmail.com>
2014-09-04 10:32:18 -0300

    Fluxo de Missões

---
+++ GameficationSenac/GameficationSenac/WCIMissao.h
@@ -0,0 +1,13 @@
+//
+//  WCIMissao.h
+//  GameficationSenac
+//
+//  Created by Danilo Makoto Ikuta on 04/09/14.
+//  Copyright (c) 2014 Danilo Makoto Ikuta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCIMissao : NSObject
+
+@end
---
+++ GameficationSenac/GameficationSenac/WCIMissao.m
@@ -0,0 +1,13 @@
+//
+//  WCIMissao.m
+//  GameficationSenac
+//
+//  Created by Danilo Makoto Ikuta on 04/09/14.
+//  Copyright (c) 2014 Danilo Makoto Ikuta. All rights reserved.
+//
+
+#import "WCIMissao.h"
+
+@implementation WCIMissao
+
+@end
