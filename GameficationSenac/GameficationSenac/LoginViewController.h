5591c01bc7ce3bfe0b263d5f9aa5ac57cbecf00e
Ezequiel <ezequiel.ifsp@gmail.com>
2014-09-04 09:23:18 -0300

    Login com video

--- GameficationSenac/GameficationSenac/LoginViewController.h
+++ GameficationSenac/GameficationSenac/LoginViewController.h
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 //#import "MissoesViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @interface LoginViewController : UIViewController
+
+@property (nonatomic, strong) AVPlayer *avplayer;
+@property (strong, nonatomic) IBOutlet UIView *movieView;
+@property (strong, nonatomic) IBOutlet UIView *gradientView;
+@property (strong, nonatomic) IBOutlet UIView *contentView;
 
 - (IBAction)botaoLogin:(id)sender;
 
--- GameficationSenac/GameficationSenac/LoginViewController.m
+++ GameficationSenac/GameficationSenac/LoginViewController.m
@@ -26,7 +26,58 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *moviePath = [bundle pathForResource:@"xx" ofType:@"mp4"];
+    NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+    
+    AVAsset *avAsset = [AVAsset assetWithURL:movieURL];
+    AVPlayerItem *avPlayerItem =[[AVPlayerItem alloc]initWithAsset:avAsset];
+    self.avplayer = [[AVPlayer alloc]initWithPlayerItem:avPlayerItem];
+    AVPlayerLayer *avPlayerLayer =[AVPlayerLayer playerLayerWithPlayer:self.avplayer];
+    [avPlayerLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [avPlayerLayer setFrame:self.view.frame];
+    [self.movieView.layer addSublayer:avPlayerLayer];
+    
+    //Not affecting background music playing
+    NSError *sessionError = nil;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:&sessionError];
+    [[AVAudioSession sharedInstance] setActive:YES error:&sessionError];
+    
+    [self.avplayer seekToTime:kCMTimeZero];
+    [self.avplayer setVolume:0.0f];
+    [self.avplayer setActionAtItemEnd:AVPlayerActionAtItemEndNone];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[self.avplayer currentItem]];
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.gradientView.bounds;
+    gradient.colors = [NSArray arrayWithObjects:(id)[UIColorFromRGB(0x030303) CGColor], (id)[[UIColor clearColor] CGColor], (id)[UIColorFromRGB(0x030303) CGColor],nil];
+    [self.gradientView.layer insertSublayer:gradient atIndex:0];
+
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.avplayer pause];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.avplayer play];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+}
+
+- (void)playerItemDidReachEnd:(NSNotification *)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
 }
 
 - (void)didReceiveMemoryWarning
